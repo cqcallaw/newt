0d52825553c5550be060e0cfcfa657b2d21a0dda
Caleb Callaway <cqcallaw@gmail.com>
2015-05-16 23:38:16 -0700

    More useful test infrastructure

--- src/newt.cpp
+++ src/newt.cpp
@@ -27,6 +27,5 @@ int main(int argc, char *argv[]) {
 		return 2;
 	}
 
-	int parse_result = yyparse();
-	return parse_result;
+	yyparse();
 }
---
+++ test.mk
@@ -0,0 +1,17 @@
+TEST_PATH = ../tests/
+
+#ref: http://stackoverflow.com/a/2706067/577298
+TEST_FILES = $(wildcard $(TEST_PATH)*.nwt)
+TESTS = $(patsubst $(TEST_PATH)%.nwt,%,$(TEST_FILES))
+
+test: newt $(TESTS)
+
+$(TEST_PATH)output:
+	mkdir -p $(TEST_PATH)output
+
+%: $(TEST_PATH)%.nwt $(TEST_PATH)output
+	./newt $< &>$(TEST_PATH)output/$@ 
+	diff $(TEST_PATH)reference/$@ $(TEST_PATH)output/$@
+
+test-clean:
+	rm -rf $(TEST_PATH)output/*
