b4431a2a2652e7ed332e3ec195ee8051608428bd
Caleb Callaway <cqcallaw@gmail.com>
2017-12-27 16:17:18 -0800

    Update release build

--- Release/src/expressions/subdir.mk
+++ Release/src/expressions/subdir.mk
@@ -6,42 +6,57 @@
 CPP_SRCS += \
 ../src/expressions/arithmetic_expression.cpp \
 ../src/expressions/binary_expression.cpp \
+../src/expressions/close_expression.cpp \
 ../src/expressions/comparison_expression.cpp \
 ../src/expressions/constant_expression.cpp \
 ../src/expressions/default_value_expression.cpp \
 ../src/expressions/expression.cpp \
 ../src/expressions/function_expression.cpp \
+../src/expressions/get_byte_expression.cpp \
 ../src/expressions/invoke_expression.cpp \
 ../src/expressions/logic_expression.cpp \
+../src/expressions/open_expression.cpp \
+../src/expressions/put_byte_expression.cpp \
 ../src/expressions/unary_expression.cpp \
+../src/expressions/using_expression.cpp \
 ../src/expressions/variable_expression.cpp \
 ../src/expressions/with_expression.cpp 
 
 OBJS += \
 ./src/expressions/arithmetic_expression.o \
 ./src/expressions/binary_expression.o \
+./src/expressions/close_expression.o \
 ./src/expressions/comparison_expression.o \
 ./src/expressions/constant_expression.o \
 ./src/expressions/default_value_expression.o \
 ./src/expressions/expression.o \
 ./src/expressions/function_expression.o \
+./src/expressions/get_byte_expression.o \
 ./src/expressions/invoke_expression.o \
 ./src/expressions/logic_expression.o \
+./src/expressions/open_expression.o \
+./src/expressions/put_byte_expression.o \
 ./src/expressions/unary_expression.o \
+./src/expressions/using_expression.o \
 ./src/expressions/variable_expression.o \
 ./src/expressions/with_expression.o 
 
 CPP_DEPS += \
 ./src/expressions/arithmetic_expression.d \
 ./src/expressions/binary_expression.d \
+./src/expressions/close_expression.d \
 ./src/expressions/comparison_expression.d \
 ./src/expressions/constant_expression.d \
 ./src/expressions/default_value_expression.d \
 ./src/expressions/expression.d \
 ./src/expressions/function_expression.d \
+./src/expressions/get_byte_expression.d \
 ./src/expressions/invoke_expression.d \
 ./src/expressions/logic_expression.d \
+./src/expressions/open_expression.d \
+./src/expressions/put_byte_expression.d \
 ./src/expressions/unary_expression.d \
+./src/expressions/using_expression.d \
 ./src/expressions/variable_expression.d \
 ./src/expressions/with_expression.d 
 
